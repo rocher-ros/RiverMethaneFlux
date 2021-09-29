bdd04f326f40852aeb6e28508e30dd6eb5f739a0
rocher-ros <g.rocher.ros@gmail.com>
2021-09-29 11:57:45 +0200

    setup

---
+++ code/0_get_files.R
@@ -0,0 +1,10 @@
+library(googledrive)
+
+
+drive_download(
+  "SCIENCE/PROJECTS/RiverMethaneFlux/methane/MethDB_tables_converted.rda",
+  path = "data/raw/MethDB_tables_converted.rda",
+  overwrite = TRUE
+)
+
+
