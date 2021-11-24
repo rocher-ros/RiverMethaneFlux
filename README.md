01ac0863cbf24da1c4514e1cdefeaa42215fdac9
rocher-ros <g.rocher.ros@gmail.com>
2021-11-24 13:20:17 +0100

    fixed figures

--- README.md
+++ README.md
@@ -1,49 +1,95 @@
 # Global estimate of Riverine Methane Flux
 
-Ongoing project trying to quantify CH4 emissions from rivers globally and monthly. 
-
+Ongoing project trying to quantify CH4 emissions from rivers globally and monthly.
 
 ## Setup
-Currently the datasets are private in a google drive, so you need access to run the code in this repository. 
-Code is in the `code/` folder. A summary of the code chunks is as follows. Each script is self-enclosed so welcome to jump in at any of them.
 
- - Script 1 downloads the hydrology layers and gets the IDs for the methane sites.
- - Script 2 obtains some new attributes: land cover, groundwater table depth and nutrient fluxes.
- - Script 3 combines all attributes into two files, one for the modelling with only the sites with CH4 concentrations and another with the 2 million catchments for upscaling.
- - Script 4 is the random forest modelling to predict CH4 concentrations globally.
- - Script 5 is to extract seasonal hydrological parameters and estimate k.
- - Script 6 is to calculate fluxes for all GRADES reaches and extrapolate to smaller streams.
- - Script 7 is to do some  maps.
+Currently the datasets are private in a google drive, so you need access to run the code in this repository. Code is in the `code/` folder. A summary of the code chunks is as follows. Each script is self-enclosed so welcome to jump in at any of them.
 
+-   Script 1 downloads the hydrology layers and gets the IDs for the methane sites.
+-   Script 2 obtains some new attributes: land cover, groundwater table depth and nutrient fluxes.
+-   Script 3 combines all attributes into two files, one for the modelling with only the sites with CH4 concentrations and another with the 2 million catchments for upscaling.
+-   Script 4 is the random forest modelling to predict CH4 concentrations globally.
+-   Script 5 is to extract seasonal hydrological parameters and estimate k.
+-   Script 6 is to calculate fluxes for all GRADES reaches and extrapolate to smaller streams.
+-   Script 7 is to do some maps and figures.
 
 ## Modelling
+
 ### Pre-processing
+
 The predictors selected are shown below. Several have been removed due to high correlation among other ones (e.g. GPP and NPP, heterotrophic soil respiration and total soil respiration, several soil properties...)
 
 ![](figures/histograms_transformed.png)
 
-
 ### Model performance
-I did some grid-tuning to find the optimal hyperparameters for the random forest model, ending up with 
-`mtry = 10, trees = 1200, min_n = 21`. I also tried running a model with the whole dataset, or an independent RF model for each month.
 
-The random forest model seems to do OK, with an average $R^{2}= 0.5$. 
+I did some grid-tuning to find the optimal hyperparameters for the random forest model, ending up with `mtry = 10, trees = 1200, min_n = 21`. I also tried running a model with the whole dataset, or an independent RF model for each month.
 
-One problem I find is that monthly models are often very different among them, given that the number of sites changes a lot month to month as well as the spatial coverage. Thus it not only captures seasonal changes but also the  (changing) geographical coverage of the data. One solution to maintain spatial coherence as well as the seasonality is for each month, feed the data of the given month and the adjacent months (e.g. for march use the data form february, march and april). This makes the model more homogeneous and robust, as well as an increase in predictive power. 
+The random forest model seems to do OK, with an average $R^{2}= 0.5$.
+
+One problem I find is that monthly models are often very different among them, given that the number of sites changes a lot month to month as well as the spatial coverage. Thus it not only captures seasonal changes but also the (changing) geographical coverage of the data. One solution to maintain spatial coherence as well as the seasonality is for each month, feed the data of the given month and the adjacent months (e.g. for march use the data form february, march and april). This makes the model more homogeneous and robust, as well as an increase in predictive power.
 
 ![](figures/model_perf_monthly_adjacent.png)
 
 We can also look at the importance of the model variables. Here I show the average (+- SE) from all the 12 models used.
 
-
 ![](figures/VIP_scores_monthly.png)
 
-Another way to look at the role of each model is at partial dependence plots of each variable. Which shows the marginal response independently. it does not take into account potential interactions though.
+Another way to look at the role of each model is at partial dependence plots of each variable. Which shows the marginal response independently. it does not take into account potential interactions though. Plots sroted from most important variable (top-left) to least (bottom-roght)
 
 ![](figures/partial_depend_monthly.png)
 
 ## Upscaling
 
+GRiMeDB has several thousand measured fluxes, so we can take advantage of this data to validate the predicted fluxes. I do this by matching predicted flux values to the corresponding flux observation for a given month.
+
+![](figures/flux_comp_boxplot.png)
+
+I can also back-calculate k from observations by dividing flux (mmol m<sup>-2</sup>d<sup>-1</sup>) with the associated concentration (mmol m<sup>-3</sup>, which gives me a kCH<sub>4</sub> in m d<sup>-1</sup>. This might help to see where the noise comes from.
+
+![](figures/flux_comp_allk.png)
+
+Things are on the same ballpark, which is a good start. Color shows how different predicted k values, seems like the values more off from the 1:1 line are sites with k differing a lot. A couple things to take into account here:
+
+-   Modelled k is estimated from estimated slope and modelled discharge, long-term averaged monthly value, for a several kilometres river reach, using a hydraulic equation.
+
+-   Measured k contains a mix of k estimates, most often derived from chambers, some slug gas releases, some from (multiple) hydraulic equations. So a wild mix here, and in the case of the chamber estimates is a quite local k estimate compared to a reach estimate. Ah, and it also flux dependent so it may be overestimated due to ebullition.
+
+So this is a bit of an apples with oranges thing here. If we restrict the comparison of modelled vs predicted fluxes to sites with similar k (+/- 50%), things actually look quite neat.
+
+![](figures/flux_comp_similark.png)
+
 ## Problems?
 
+### Spatial and temporal data aggregation
 
+One thing to think about is how data is included in the model. On one hand, GrRiMeDB has thousands of sites with one observation but also a handful of sites (Site_Nid) with dozens of observations (over multiple years in some cases. In the other hand the modelling is done using GRADES, whose sites (COMID) are a bit coarser spatially and at a monthly scale.
+
+This means that some sites have a larger influence in the model. For example, at my backyard we have the Krycklan Catchment Study, with \~15 sites with monthly data for \~10 years, and a spatial survey with more than 100 sites. In the image below all sites are in red and in blue the nearest GRADES reaches.
+
+![](figures/krycklan.JPG)
+
+You see that the GRADES network reaches the main stem of the network, but most of the sites are snapped to the catchment outlet. This means that for this GRADES reach we have more than 100 sites and more than 1000 observations. Some of those sites are draining contrasting catchments with distinct methane dynamics (See Gomez-Gener, Hotchkiss, Laudon and Sponseller 2021 WRR), and it all gets lumped into one reach with homogeneous characteristics. Here is a summary of CH4 concentrations for all those sites.
+
+![](figures/site_krycklan.png)
+
+This affects the model in two ways. On one side it overfits because same sites are likely to be both in the training and test datasets. On the other side the model will struggle to perform well as concentrations range three orders of magnitude but the predictors are more or less constant.
+
+A solution here would be to aggregate data by a GRADES reach and month, another solution is to throw away sites that are snapped more than x m.
+
+### k, my little obsession
+
+So I included k in the model, and arises as an important predictor of CH4 concentrations. A good check is to look at how modelled k relates to modelled CH4.
+
+![](figures/k_ch4_predicted.png)
+
+So far so good. That's what it should be. The problem however is that k ranges up to 2000 m d^-1. I am not saying that such high k are impossible, but the marginal effect on the flux should be negligible. In other words, CH4 is still predicted to be above saturation, but the CH4 flux at 50 vs 2000 m d^-1 should be relatively similar and not 40 times higher.
+
+This is more apparent when we look at k vs flux, and we see that higher fluxes are at higher k.
+
+![](figures/k_flux_predicted.png)
+
+This is not the relationship we see on the empirical data though. First plot is for the sites I could back-calculate k , and second plot is more data with reach slope instead of k.
+
+![](figures/k_flux_observed.png)
--- code/x_figures.R
+++ code/x_figures.R
@@ -5,7 +5,7 @@
 
 # 0. Load  and install packages ----
 # List of all packages needed
-package_list <- c('tidyverse', 'googledrive' , 'sf',  'ggpubr', 'rnaturalearth')
+package_list <- c('tidyverse', 'googledrive' , 'sf',  'ggpubr', 'rnaturalearth', 'ggtext', 'lubridate')
 
 # Check if there are any packacges missing
 packages_missing <- setdiff(package_list, rownames(installed.packages()))
@@ -27,7 +27,8 @@ grades <-  read_csv("data/raw/gis/GRADES_attributes/grades_coords.csv", lazy = F
 
 
 #turn it an sf object
-meth_gis <- left_join(meth_concs, grades, by = "COMID") %>% 
+meth_gis <- left_join(meth_concs, grades, by = "COMID") %>%
+  #mutate(across(ends_with("_k"), ~ifelse(.x > 50, 50, .x))) %>% 
   mutate(Jan_fch4 = (Jan_ch4 - Jan_ch4eq)*Jan_k,
          Feb_fch4 = (Feb_ch4 - Feb_ch4eq)*Feb_k,
          Mar_fch4 = (Mar_ch4 - Mar_ch4eq)*Mar_k,
@@ -82,7 +83,7 @@ meth_hexes <- st_join(meth_avg, grid, join = st_intersects)
 meth_hexes_avg <- meth_hexes %>% 
   group_by(index) %>%
   summarise(methane = mean(ch4_mean, na.rm = TRUE),
-            methane.flux = sum(Fch4_mean, na.rm = TRUE),
+            methane.flux = mean(Fch4_mean, na.rm = TRUE),
             runoff= mean(runoff, na.rm = TRUE) ) %>%
   st_sf()  
 
@@ -117,32 +118,57 @@ ggplot() +
     option = "magma", na.value = "gray",
     direction = -1,
     #trans = "log10", 
-    name = "Methane concentration (umol/L)")+
+    name = "**Methane concentration** <br>(mmol m^-3 )")+
   guides( fill = guide_colourbar(title.position = "top"))+
   theme_void()+
-  theme(legend.position = c(0.55, 0.15),
-        legend.direction = "horizontal")
+  theme(legend.position = c(0.55, 0.18),
+        legend.direction = "horizontal",
+        legend.title = ggtext::element_markdown(size = 14),
+        legend.text = element_text(size=12),
+        legend.key.size = unit(.9, 'cm'))
 
 ggsave(filename = "figures/map_ch4.png", dpi=600, height = 10, width = 16)
 
 
 ggplot() +
   geom_sf(
-    data = meth_hexes_avg2 %>%  mutate(methane.flux= ifelse(methane.flux > 5000,5000, methane.flux)), 
-                                       color = NA, aes( fill = methane.flux) )+
+    data = meth_hexes_avg2, aes(fill = methane.flux), color = NA )+
   geom_sf(data=lakes %>% filter(scalerank < 2), fill="aliceblue", color=NA)+
   scale_fill_viridis_c(
     option = "magma", na.value = "gray",
-    #trans = "log10", 
+    trans = "pseudo_log", 
+    breaks= c(0,1,5,10,30),
     direction = -1,
-    name = "Methane flux")+
+    name = "**Methane flux** <br> (mmol m^-2 d^-1 )")+
   guides( fill = guide_colourbar(title.position = "top"))+
   theme_void()+
-  theme(legend.position = c(0.55, 0.15),
-        legend.direction = "horizontal")
+  theme(legend.position = c(0.55, 0.18),
+        legend.direction = "horizontal",
+        legend.title = ggtext::element_markdown(size = 14),
+        legend.text = element_text(size=12),
+        legend.key.size = unit(.9, 'cm'))
 
 ggsave(filename = "figures/map_ch4_flux.png", dpi=600, height = 10, width = 16)
 
+
+ggplot(meth_avg, aes(k_mean, ch4_mean))+
+  geom_hex(bins=50)+
+ # scale_x_log10(labels=scales::number)+
+  #scale_y_log10(labels=scales::number)+
+  scale_fill_viridis_c()+
+  theme_bw()
+
+
+ggsave("figures/k_ch4_predicted.png")
+
+ggplot(meth_avg, aes(k_mean, Fch4_mean))+
+  geom_hex(bins=50)+
+  # scale_x_log10(labels=scales::number)+
+  #scale_y_log10(labels=scales::number)+
+  scale_fill_viridis_c()+
+  theme_bw()
+
+ggsave("figures/k_flux_predicted.png")
 
 
 ch4E_hybas <- read_csv("output/ch4E_hybas.csv")
@@ -154,38 +180,172 @@ ch4E_hybas %>%
   summarise(sum=sum(per_day))
   
 
+## Flux comparison with grime ----
+# Load grime DB
+load(file.path("data", "raw", "MethDB_tables_converted.rda"))
 
-median(meth_avg$Fch4_mean, na.rm=TRUE)
+
+# and the fiel with the COMID from GRADES and SiteNId from GRIME
+grime_comids <- read_csv("data/processed/sites_meth_comid.csv") %>% 
+  mutate(Site_Nid= as.character(Site_Nid))
+
+#merge the files
+sites_clean <- sites_df %>% 
+  left_join(grime_comids, by="Site_Nid") %>% 
+  drop_na(COMID)
+
+#get the flux df and fix some issues, also merge with conc_Df
+flux_comid <-  flux_df %>% 
+  mutate(Diffusive_CH4_Flux_Mean =ifelse(Diffusive_CH4_Flux_Mean < 0, 0, Diffusive_CH4_Flux_Mean)) %>% 
+  filter(Site_Nid %in% sites_clean$Site_Nid) %>% 
+  left_join(sites_clean, by="Site_Nid") %>% 
+  mutate(k_method= str_replace(k_method, "additon", "addition"),
+         k_method = str_replace(k_method, "Physical" ,"physical")) %>% 
+  left_join(conc_df, by=c("Site_Nid", "Date_start"))
+
+#compare mean fluxes observed and predicted
+mean(meth_avg$Fch4_mean, na.rm=TRUE)
+mean(flux_comid$Diffusive_CH4_Flux_Mean, na.rm=TRUE)
+
+#now join the observed fluxes and predicted into one df. I will filter the sites that are not well snapped (>100m)
+flux_comp <- flux_comid %>% 
+  select(COMID, Site_Nid, date= Date_start, distance_snapped, CH4mean, Diffusive_CH4_Flux_Mean, Diff_Method, k_method ) %>% 
+  filter(distance_snapped < 100) %>% 
+  left_join( meth_gis %>% st_drop_geometry(), by="COMID") %>% 
+  drop_na(Diffusive_CH4_Flux_Mean) %>% 
+  mutate(k_obs =Diffusive_CH4_Flux_Mean/CH4mean,  # estimate kch4 (m d-1) as the quotient of flux (mmol m-2 d-1) and concentration (mmol m-3)
+         month_obs= month(date),
+         k_pred =  case_when(month_obs == 1 ~  Jan_k,
+                             month_obs == 2 ~  Feb_k,
+                             month_obs == 3 ~  Mar_k,
+                             month_obs == 4 ~  Apr_k,
+                             month_obs == 5 ~  May_k,
+                             month_obs == 6 ~  Jun_k,
+                             month_obs == 7 ~  Jul_k,
+                             month_obs == 8 ~  Aug_k,
+                             month_obs == 9 ~  Sep_k,
+                             month_obs == 10 ~ Oct_k,
+                             month_obs == 11 ~ Nov_k,
+                             month_obs == 12 ~ Dec_k),
+         flux_pred = case_when(month_obs == 1 ~  Jan_fch4,
+                               month_obs == 2 ~  Feb_fch4,
+                               month_obs == 3 ~  Mar_fch4,
+                               month_obs == 4 ~  Apr_fch4,
+                               month_obs == 5 ~  May_fch4,
+                               month_obs == 6 ~  Jun_fch4,
+                               month_obs == 7 ~  Jul_fch4,
+                               month_obs == 8 ~  Aug_fch4,
+                               month_obs == 9 ~  Sep_fch4,
+                               month_obs == 10 ~ Oct_fch4,
+                               month_obs == 11 ~ Nov_fch4,
+                               month_obs == 12 ~ Dec_fch4)
+         ) 
 
 
-ggplot(meth_avg, aes(ch4_mean, Fch4_mean))+
-  geom_hex(bins=50)+
+flux_comp %>% filter( !k_method %in% c("not determined", "other"),
+                       flux_pred > 0, Diffusive_CH4_Flux_Mean > 0, is.na(k_obs) == FALSE) %>%  
+  ggplot(aes(Diffusive_CH4_Flux_Mean, flux_pred, color=(k_pred/k_obs)*100), alpha=.5, size=2)+
+  geom_point()+
+  geom_abline(slope=1, intercept = 0)+
+  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")))+
+  scale_color_viridis_c(trans="log10", labels=scales::number, breaks = c(1,100,10000))+
   scale_x_log10(labels=scales::number)+
   scale_y_log10(labels=scales::number)+
-  scale_fill_viridis_c()+
+  labs(x="Measured CH4 diffusive flux", y="Modelled CH4 diffusive flux", 
+       color="% difference\n predicted \n vs observed k",
+       caption="Values close to 100 in the color scale mean that k is similar, 
+       values of 1000 means that predicted k is 10 times higher than observed k")+
+  theme_bw()+
+  theme(legend.key.size = unit(1, 'cm'))
+
+ggsave("figures/flux_comp_allk.png")
+
+ggplot(data= flux_comp %>% filter( !k_method %in% c("not determined", "other"),
+                                     k_pred/k_obs > .5 &  k_pred/k_obs < 1.5, flux_pred > 0),
+         aes(Diffusive_CH4_Flux_Mean, flux_pred))+
+    geom_point(data=flux_comp %>% filter( !k_method %in% c("not determined", "other"),
+                                          flux_pred > 0, Diffusive_CH4_Flux_Mean > 0, is.na(k_obs) == FALSE),
+               aes(Diffusive_CH4_Flux_Mean, flux_pred, color=(k_pred/k_obs)*100), alpha=.5, size=2)+
+  geom_point( color="black", size=2)+
+  geom_abline(slope=1, intercept = 0)+
+  geom_smooth(method="lm")+
+  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")))+
+  stat_regline_equation(label.y.npc = .9)+
+  scale_x_log10(labels=scales::number)+
+  scale_y_log10(labels=scales::number)+
+  scale_color_viridis_c(trans="log10", labels=scales::number, breaks = c(1, 50,  150, 10000))+
+  labs(x="Measured CH4 diffusive flux", y="Modelled CH4 diffusive flux", 
+       color="% difference predicted \n vs observed k",
+       caption="regression now is done only with the points that have comparable k, +/- 50%")+
+  theme_bw()+
+  theme(legend.key.size = unit(1, 'cm'))
+
+  ggsave("figures/flux_comp_similark.png")
+
+flux_comp %>% 
+  filter(k_pred > .01, k_obs > .01) %>% 
+  rename(modelled_flux = flux_pred, measured_flux = Diffusive_CH4_Flux_Mean) %>% 
+  pivot_longer(c(modelled_flux, measured_flux), values_to = "ch4_flux", names_to = "method") %>% 
+  ggplot(aes(x=method, y=ch4_flux))+
+  geom_boxplot(fill="blue3", alpha=.5)+
+  scale_y_log10(labels=scales::number)+
+  labs(x="", y="CH<sub>4</sub> flux (mmol m^-2 d^-1)")+
+  theme_bw()+
+  theme(axis.title.y = ggtext::element_markdown())
+
+ggsave("figures/flux_comp_boxplot.png")
+
+flux_comp %>% filter(k_pred > .1, k_obs > .1, !k_method %in% c("not determined", "other")) %>% 
+ggplot( aes(k_obs, k_pred))+
+  geom_point()+
+  geom_abline(slope=1, intercept = 0)+
+  geom_smooth(method="lm", se=FALSE)+
+  stat_cor()+
+  scale_x_log10(labels=scales::number)+
+  scale_y_log10(labels=scales::number)+
+  labs(x="Measured k", y="Modelled k")+
   theme_bw()
+
+ggsave("figures_comp_k.png")
+
+obs_flux_k <- flux_comp %>% 
+  filter( k_obs > .1, k_obs < 500) %>% 
+  ggplot(aes(k_obs,Diffusive_CH4_Flux_Mean))+
+  geom_point()+
+  theme_bw()+
+  labs(x="estimated k from the F/conc observations")
+
+obs_flux_slope <- flux_comid %>% 
+  ggplot(aes(slope_m_m,Diffusive_CH4_Flux_Mean))+
+  geom_point()+
+  theme_bw()+
+  labs(x="channel slope")
+
+ggarrange(obs_flux_k, obs_flux_slope, nrow = 1)
+
+ggsave("figures/k_flux_observed.png", width = 11, height = 5)
+
+#look at spatial aggregation of COMID to sites
+grimeDB_attributes <- read_csv("data/processed/grimeDB_concs_with_grade_attributes.csv") %>% 
+  filter(`Aggregated?` == "No",
+         !str_detect(Channel_type,"DD|PI|GT|PS|TH|Th|DIT")) %>%
+  mutate( month=month(date)) 
+
+sites_many_obs <- grimeDB_attributes %>% 
+  group_by(COMID) %>% 
+  summarise(n_obs=n()) %>% 
+  arrange(desc(n_obs)) %>% 
+  #filter(n_obs > 20) %>% 
+  left_join(grimeDB_attributes %>% select(COMID, Site_Nid, date, CH4mean, lat, lon))
 
 grimeDB_attributes %>% 
-  select(CH4mean) %>% 
-  mutate(type="observations") %>% 
-  bind_rows(meth_avg %>% 
-              st_drop_geometry() %>% 
-              select(CH4mean= ch4_mean) %>% 
-              mutate(type="predictions")) %>% 
-ggplot()+
-  geom_density(aes(CH4mean), alpha=.6)+
-  scale_x_log10(labels=scales::number)+
-  facet_wrap(~type, ncol = 1, scales = "free_y")+
-  theme_bw()
-
-min(grimeDB_attributes$CH4mean)
-
-meth_data <- meth_gis %>% 
-  st_drop_geometry() %>% 
-  select(COMID, Jan_ch4:Dec_ch4) %>% 
-  pivot_longer(-COMID, values_to = "CH4mean", names_to = "month")
-
-meth_data %>% 
+  filter(COMID == 24006204, CH4mean > .0001) %>% 
   ggplot()+
-  geom_density(aes(CH4mean))+
-  facet_wrap(~month)
+  geom_boxplot(aes(y=as.factor(Site_Nid), x= CH4mean))+
+  scale_x_log10(labels=scales::number)+
+  theme_bw()+
+  labs(y="Site_Nid")
+
+ggsave("figures/site_krycklan.png", width = 5, height = 10)
+
+
